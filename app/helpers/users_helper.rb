8bd4c802452bc7fe63229436929d32a79c4a9133
Connor Wright <connor.d.wright1996@gmail.com>
2020-02-14 05:45:49 +0000

    implemented signup with errors need to do tests next

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  
+  #Returns the Gravatar for the given user.
+  def gravatar_for(user, size: nil)
+    #we store emails as downcase but just in case
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, size: size , alt: user.name, class: "gravatar")
+  end
 end
