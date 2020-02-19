9e9f4fb79251d29ffb2b873d60b9493cee8677a9
Connor Wright <connor.d.wright1996@gmail.com>
2020-02-19 05:15:12 +0000

    stopping for tonight got a basic session going

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    
+    assert_response :success
+    post login_path, params: {session: {email: "", password: ""}}
+    
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    
+    get root_path
+    assert flash.empty?
+  end
+end
