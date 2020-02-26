d969fac7de49c7f673f85e6424ba31579a0b4ac5
Connor Wright <connor.d.wright1996@gmail.com>
2020-02-26 04:57:49 +0000

    finished 10.1 allowing for users to update

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255},
                     format: {with: VALID_EMAIL_REGEX}, 
                     uniqueness: true
-  validates :password, presence: true, length: {minimum: 6}
                     
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
   #remembers a user in the database for use in persistent sessions
   def remember
