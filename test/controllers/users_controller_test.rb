4b8ec9da2f26ce031f1d9b4b165ae76be60aeda3
Connor Wright <connor.d.wright1996@gmail.com>
2020-02-28 05:42:00 +0000

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   
   def setup 
-    @user       = users(:michael)
-    @other_user = users(:archer)
+    @admin       = users(:michael)
+    @non_admin = users(:archer)
   end
   
   test "should get new" do
@@ -12,31 +12,61 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
+  test "should redirect index when not logged in" do 
+    get users_path
+    assert_redirected_to login_url
+  end
+  
   test "should redirect edit when not logged in" do
-    get edit_user_path(@user)
+    get edit_user_path(@admin)
     assert_not flash.empty?
     assert_redirected_to login_url
   end
   
   test "should redirect update wehn not logged in" do
-    patch user_path(@user), params: { user: { name: @user.name,
-                                               email: @user.email }}
+    patch user_path(@admin), params: { user: { name: @admin.name,
+                                               email: @admin.email }}
     assert_not flash.empty?
     assert_redirected_to login_url
   end
   
   test "should redirect edit when logged in as wrong user" do
-    log_in_as(@other_user)
-    get edit_user_path(@user)
+    log_in_as(@non_admin)
+    get edit_user_path(@admin)
     assert flash.empty?
     assert_redirected_to root_url
   end
   
   test "should redirect update when logged in as wrong user" do
-    log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email }  }
+    log_in_as(@non_admin)
+    patch user_path(@admin), params: { user: { name: @admin.name,
+                                              email: @admin.email }  }
     assert flash.empty?
+    assert_redirected_to root_url
+  end
+  
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@non_admin)
+    assert_not @non_admin.admin?
+    patch user_path(@non_admin), params: {
+                                  user: { password:         "password",
+                                  password_confirmation:    "password",
+                                  admin: 1 } }
+    assert_not @non_admin.reload.admin?
+  end
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@admin)
+    end
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@non_admin)
+    assert_no_difference 'User.count' do
+      delete user_path(@admin)
+    end
     assert_redirected_to root_url
   end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+  
+  
+  test "logged out layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -14,5 +19,22 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+  
+  test "logged layout links" do
+    log_in_as @user
+    get root_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", users_path
+    
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", signup_path, count: 0
   end
 end
