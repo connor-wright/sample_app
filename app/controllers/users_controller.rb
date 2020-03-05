05b0e8b2e9636215aa75580fdd2e96a465d6105c
Connor Wright <connor.d.wright1996@gmail.com>
2020-03-05 04:38:55 +0000

    finished section 13.3 added a feed and the ability to create microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,14 +56,7 @@ class UsersController < ApplicationController
   
   end
   
-  #before filters confirms logged in user
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in"
-      redirect_to login_url
-    end
-  end
+  # Before filters
   
   #confirms the correct user
   def correct_user
